b50e082fe01cf661e2bc835c6cf4a057d8a066d8
Camden Segal <camden.segal@gmail.com>
2014-06-23 17:07:36 -0400

    Minnified scripts and styles when not WP_DEBUG, remove customizer from concat-ed scripts

--- themes/_s/functions.php
+++ themes/_s/functions.php
@@ -92,13 +92,16 @@ add_action( 'widgets_init', '_s_widgets_init' );
  * Enqueue scripts and styles.
  */
 function _s_scripts() {
+	$minnified = '.min';
+	if ( defined( 'WP_DEBUG' ) && WP_DEBUG == true ) {
+		$minnified = '';
+	}
+
 	wp_register_style( 'font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css', array(), null );
 	wp_enqueue_style( 'font-awesome' );
-	wp_enqueue_style( '_s-style', get_stylesheet_uri() );
+	wp_enqueue_style( '_s-style', get_stylesheet_directory_uri() . '/style' . $minnified . '.css' );
 
-	wp_enqueue_script( '_s-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20120206', true );
-
-	wp_enqueue_script( '_s-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20130115', true );
+	wp_enqueue_script( '_s-project', get_template_directory_uri() . '/js/project' . $minnified . '.js', array('jquery'), '20120206', true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
