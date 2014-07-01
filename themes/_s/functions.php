f4910d670d813a78eebb7b46c01d687f088de9b8
Greg Rickaby <gregrickaby@gmail.com>
2014-07-01 17:18:02 -0500

    fast forward _s template files

--- themes/_s/functions.php
+++ themes/_s/functions.php
@@ -44,7 +44,7 @@ function _s_setup() {
 	register_nav_menus( array(
 		'primary' => __( 'Primary Menu', '_s' ),
 	) );
-
+	
 	/*
 	 * Switch default core markup for search form, comment form, and comments
 	 * to output valid HTML5.
@@ -92,19 +92,11 @@ add_action( 'widgets_init', '_s_widgets_init' );
  * Enqueue scripts and styles.
  */
 function _s_scripts() {
-	// Should we use minified or unminified files?
-	$minnified = '.min';
-	if ( defined( 'SCRIPT_DBEUG' ) && SCRIPT_DBEUG == true ) {
-		$minnified = '';
-		// Also enqueue live reload when developing to allow for extensionless reloading!
-		wp_enqueue_script( 'live-reload', '//localhost:35729/livereload.js', array(), '1', true );
-	}
+	wp_enqueue_style( '_s-style', get_stylesheet_uri() );
 
-	wp_register_style( 'font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css', array(), null );
-	wp_enqueue_style( 'font-awesome' );
-	wp_enqueue_style( '_s-style', get_stylesheet_directory_uri() . '/style' . $minnified . '.css' );
+	wp_enqueue_script( '_s-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20120206', true );
 
-	wp_enqueue_script( '_s-project', get_template_directory_uri() . '/js/project' . $minnified . '.js', array('jquery'), '20120206', true );
+	wp_enqueue_script( '_s-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20130115', true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
